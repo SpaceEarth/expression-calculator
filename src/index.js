bd82c5317e264e5b313514ca2552579c672103fa
yasyuchenya <alexyasyuchenya@gmail.com>
2019-09-25 00:16:26 +0300

    impl: expressionCalculator

--- src/index.js
+++ src/index.js
@@ -3,44 +3,66 @@ function eval() {
     return;
 }
 
-function getPriority(value) {
-    switch (value) {
-        case '-':
-        case '+':
-            return 1;
-        case '*':
-        case '/':
-            return 2;
-        case '(':
-            return getPriority.base += 1;
-        case ')':
-            return getPriority.base -= 1;
+function calculate(arr) {
+    switch (arr[1]) {
+        case '+': return +arr[0] + +arr[2];
+        case '-': return arr[0] - arr[2];
+        case '*': return arr[0] * arr[2];
+        case '/': 
+            if (+arr[2] === 0) throw new TypeError("TypeError: Devision by zero.");
+            return arr[0] / arr[2];
     }
-    return 0;
 }
-getPriority.base = 2;
+
+function calculateResult(array) {
+    let openNum,
+        closeNum,
+        signIndx;
+
+    while (array.indexOf('(') >= 0) {
+        array.some((v, i) => {
+            if (v === '(') {
+                openNum = i;
+            } else if (v === ')') {
+                closeNum = i;
+                return true;
+            }
+            return false;
+        });
+        array.splice(openNum, closeNum - openNum + 1, calculateResult(array.slice(openNum + 1, closeNum)));
+    }
+
+    while (array.length > 1) {
+        signIndx = undefined;
+        array.some((v, i) => {
+            if (!signIndx && (v === '+' || v === '-')) {
+                signIndx = i;
+            } else if (v === '*' || v === '/') {
+                signIndx = i;
+                return true;
+            }
+            return false;
+        });
+        array.splice(signIndx - 1, 3, calculate(array.slice(signIndx - 1, signIndx + 2)));
+    }
+
+    return array[0];
+}
 
 function expressionCalculator(expr) {
-    console.log([...expr.replace(/ /g, "")].reduce((acc, value, indx) => {
-        if (isNaN(Number(value))) {
-            acc.stack.push(value);
-            acc.priority.push(getPriority(value));
-            acc.last = false;
-        } else {
-            if (acc.last) {
-                acc.stack[acc.stack.length - 1] += value;
-            } else {
-                acc.stack.push(value);
-                acc.priority.push(0);
-            }
-            acc.last = true;
-        } 
+    const [open, close] = [...expr].reduce((acc, v) => {
+        if (v === '(') acc[0] += 1;
+        if (v === ')') acc[1] += 1;
         return acc;
-    }, {
-        stack: [],
-        priority: [],
-        last: false
-    }).priority);
+    }, [0, 0]);
+
+    if (open !== close) {
+        throw new Error("ExpressionError: Brackets must be paired");
+    }
+
+    return calculateResult(expr
+        .replace(/ /g, "")
+        .match(/[0-9]{1,}|.{1}/g));
 }
 
 module.exports = {
